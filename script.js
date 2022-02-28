2641d27a6d3e8b366ccbf7b13a8f7056a7071e45
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-02-28 18:03:47 -0400

    Requirement 3 complete! :heavy_check_mark: Added login form

---
+++ script.js
@@ -0,0 +1,13 @@
+const buttonLogin = document.querySelector('#button');
+
+function validation() {
+  const email = document.querySelector('#email');
+  const password = document.querySelector('#password');
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+buttonLogin.addEventListener('click', validation);
