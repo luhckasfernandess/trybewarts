b85e0b6cb6852cb9355d20d0f3c281c18f20397f
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-03-01 10:45:57 -0400

    Requirement 5 complete! :heavy_check_mark: Form added in the body of the page

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const buttonLogin = document.querySelector('#button');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
-const input = document.querySelector('.input');
 
 function validation() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
